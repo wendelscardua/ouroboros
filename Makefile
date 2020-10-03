d90826da49d26af2956b30f0212cf19881dbbf7f
Wendel Scardua <wendelscardua@gmail.com>
2020-10-03 01:21:11 -0300

    First commit (template extracted from wendelscardua/planter)

---
+++ Makefile
@@ -0,0 +1,55 @@
+PROJECT=ouroboros
+LD65_FLAGS=
+CA65_FLAGS=
+NSF2DATA=/mnt/c/NESDev/famitone2d/NSF/nsf2data.exe
+TEXT2DATA=/mnt/c/NESDev/famitone2d/text2data.exe
+FAMITRACKER=/mnt/c/NESDev/famitracker/FamiTracker.exe
+EMULATOR=/mnt/c/Games/fceux/fceux.exe
+
+TARGET=${PROJECT}.nes
+
+.PHONY : debug run
+
+default: ${TARGET}
+
+${TARGET}: src/${PROJECT}.o src/reset.o src/readjoy.o src/unrle.o src/rand.o src/audio-data.o
+	ld65 $^ -t nes -o ${TARGET} ${LD65_FLAGS}
+
+debug: LD65_FLAGS += -Ln labels.txt --dbgfile ${PROJECT}.nes.dbg
+debug: CA65_FLAGS += -g -DDEBUG=1
+debug: ${TARGET}
+
+src/${PROJECT}.o: src/${PROJECT}.s src/constants.inc src/mmc3-constants.inc src/header.inc \
+	src/circle-lut.inc \
+	src/famitone2.s \
+	assets/bg-palettes.pal assets/sprite-palettes.pal \
+	assets/nametables/*.rle \
+	assets/chr/*.chr
+	ca65 src/${PROJECT}.s ${CA65_FLAGS}
+
+src/audio-data.o: src/audio-data.s assets/audio/sfx.s assets/audio/soundtrack.s
+	ca65 src/audio-data.s ${CA65_FLAGS}
+
+assets/audio/soundtrack.s: assets/audio/soundtrack.txt
+	${TEXT2DATA} $^ -ca65 -allin
+
+assets/audio/soundtrack.txt: assets/audio/soundtrack.ftm
+	${FAMITRACKER} $^ -export $@
+
+assets/audio/sfx.nsf: assets/audio/sfx.ftm
+	${FAMITRACKER} assets/audio/sfx.ftm -export assets/audio/sfx.nsf
+
+assets/audio/sfx.s: assets/audio/sfx.nsf
+	${NSF2DATA} assets/audio/sfx.nsf -ca65 -ntsc
+
+src/circle-lut.inc: tools/generate-circle-lut.rb
+	ruby tools/generate-circle-lut.rb > src/circle-lut.inc
+
+%.o: %.s
+	ca65 $< ${CA65_FLAGS}
+
+clean:
+	rm src/*.o *.nes labels.txt *.dbg
+
+run: debug
+	${EMULATOR} ${PROJECT}.nes
---
+++ src/ouroboros.s
@@ -0,0 +1,521 @@
+.include "constants.inc"
+.include "mmc3-constants.inc"
+.include "header.inc"
+
+.feature force_range
+.linecont +
+
+; famitone2 config
+FT_PAL_SUPPORT=0
+FT_NTSC_SUPPORT=1
+FT_SFX_ENABLE=1
+FT_THREAD=1
+FT_DPCM_ENABLE=0
+FT_SFX_STREAMS=4
+FT_DPCM_OFF=$c000
+
+; music/sfx constants
+.enum music_track
+.endenum
+
+.enum sfx
+.endenum
+
+.macro SFX effect, channel
+  save_regs
+  LDA #sfx::effect
+  LDX #.ident ( .concat( "FT_SFX_", .string(channel) ) )
+  JSR FamiToneSfxPlay
+  restore_regs
+.endmacro
+
+.macro PLAY track
+.local skip
+  save_regs
+  LDA #music_track::track
+  JSR FamiToneMusicPlay
+  restore_regs
+.endmacro
+
+.macro PRINT string, ppuaddr
+  save_regs
+  LDA #<(string)
+  STA addr_ptr
+  LDA #>(string)
+  STA addr_ptr+1
+  LDA #<(ppuaddr)
+  STA ppu_addr_ptr
+  LDA #>(ppuaddr)
+  STA ppu_addr_ptr+1
+  JSR write_string
+  restore_regs
+.endmacro
+
+.macro SCREEN_OFF
+  LDA #$00
+  STA PPUCTRL ; disable NMI
+  STA PPUMASK ; disable rendering
+.endmacro
+
+.macro SCREEN_ON
+  LDA #%10001000  ; turn on NMIs, sprites use second pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
+  STA PPUMASK
+.endmacro
+
+.macro VERTICAL_PPUADDR
+  LDA #%10001100
+  STA PPUCTRL
+.endmacro
+
+.macro HORIZONTAL_PPUADDR
+  LDA #%10001000
+  STA PPUCTRL
+.endmacro
+
+; game config
+
+.segment "ZEROPAGE"
+FT_TEMP: .res 3
+.segment "FAMITONE"
+FT_BASE_ADR: .res 186
+.segment "CODE"
+.include "famitone2.s"
+
+.segment "OAM"
+.struct Sprite
+  ycoord .byte
+  tile .byte
+  flag .byte
+  xcoord .byte
+.endstruct
+
+oam_sprites:
+  .repeat 64
+    .tag Sprite
+  .endrepeat
+.zeropage
+
+.importzp buttons
+.importzp last_frame_buttons
+.importzp released_buttons
+.importzp pressed_buttons
+.importzp rng_seed
+.importzp rle_ptr
+
+; zp vars
+addr_ptr: .res 2 ; generic address pointer
+ppu_addr_ptr: .res 2
+
+nmis: .res 1
+old_nmis: .res 1
+
+sprite_counter: .res 1
+
+.enum game_states
+  waiting_to_start
+  playing
+  game_over
+.endenum
+
+game_state: .res 1
+
+.segment "BSS"
+; non-zp RAM goes here
+
+.segment "CODE"
+
+.import reset_handler
+.import readjoy
+.import rand
+.import unrle
+
+.import music_data
+.import sfx_data
+
+.macro KIL ; pseudo instruction to kill the program
+  .byte $12
+.endmacro
+
+.macro VBLANK
+  .local vblankwait
+vblankwait:
+  BIT PPUSTATUS
+  BPL vblankwait
+.endmacro
+
+.macro save_regs
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+.endmacro
+
+.macro restore_regs
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+.endmacro
+
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  INC nmis
+  RTI
+.endproc
+
+.export main
+.proc main
+  SEI         ; ignore IRQs
+  CLD         ; disable decimal mode
+  LDX #$40
+  STX $4017   ; disable APU frame IRQ
+  LDX #$ff
+  TXS         ; Set up stack
+  INX         ; now X = 0
+  STX PPUCTRL ; disable NMI
+  STX PPUMASK ; disable rendering
+  STX $4010   ; disable DMC IRQs
+
+  LDX #0
+clear_ram:
+  LDA #$00
+  STA $0000,X
+  STA $0100,X
+  STA $0300,X
+  STA $0400,X
+  STA $0500,X
+  STA $0600,X
+  STA $0700,X
+  LDA #$fe
+  STA $0200,X
+  INX
+  BNE clear_ram
+
+  ; vertical mirroring (fix for everdrive)
+  LDA #%00000000
+  STA $a000
+
+  SCREEN_ON
+
+  LDX #<music_data
+  LDY #>music_data
+  LDA #1
+  JSR FamiToneInit
+
+  ; init FamiTone SFX
+  LDX #<sfx_data
+  LDY #>sfx_data
+  LDA #1
+  JSR FamiToneSfxInit
+
+  ; init rng
+  LDA #$a9
+  STA rng_seed
+  LDA #$73
+  STA rng_seed+1
+
+  CLI ; enable interrupts
+
+  JSR go_to_title
+
+forever:
+  LDA nmis
+  CMP old_nmis
+  BEQ etc
+  STA old_nmis
+.ifdef DEBUG
+  LDA #%10011110  ; red tint
+  STA PPUMASK
+.endif
+  JSR refresh_oam
+  ; reset ppuaddr
+  BIT PPUSTATUS
+  LDA #$20
+  STA PPUADDR
+  LDA #$00
+  STA PPUADDR
+  ; reset scroll
+  LDA #$00
+  STA PPUSCROLL
+  STA PPUSCROLL
+
+  ; new frame code
+  JSR game_state_handler
+.ifdef DEBUG
+  LDA #%00011110  ; no tint
+  STA PPUMASK
+.endif
+  JSR FamiToneUpdate
+  JSR slow_updates
+etc:
+  JMP forever
+.endproc
+
+.proc refresh_oam
+  ; Refresh OAM
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
+  RTS
+.endproc
+
+.proc load_palettes
+  ; cobbles Y
+  LDY PPUSTATUS
+  LDY #$3f
+  STY PPUADDR
+  LDY #$00
+  STY PPUADDR
+:
+  LDA palettes,Y
+  STA PPUDATA
+  INY
+  CPY #$20
+  BNE :-
+  RTS
+.endproc
+
+.proc load_default_chr
+  ; bg
+  LDA #0
+  STA BANK_SELECT
+  LDA #0
+  STA BANK_DATA
+  LDA #1
+  STA BANK_SELECT
+  LDA #2
+  STA BANK_DATA
+
+  ; sprites
+  LDA #2
+  STA BANK_SELECT
+  LDA #4
+  STA BANK_DATA
+
+  LDA #3
+  STA BANK_SELECT
+  LDA #5
+  STA BANK_DATA
+
+  LDA #4
+  STA BANK_SELECT
+  LDA #6
+  STA BANK_DATA
+
+  LDA #5
+  STA BANK_SELECT
+  LDA #7
+  STA BANK_DATA
+  RTS
+.endproc
+
+.proc game_state_handler
+  LDX game_state
+  LDA game_state_handlers_h, X
+  PHA
+  LDA game_state_handlers_l, X
+  PHA
+  RTS
+.endproc
+
+.proc slow_updates
+  JSR rand
+  RTS
+.endproc
+
+
+.proc go_to_title
+  LDA #game_states::waiting_to_start
+  STA game_state
+
+  ; erase sprites
+  LDX #$00
+  LDA #$F0
+:
+  STA oam_sprites+Sprite::ycoord, X
+  .repeat .sizeof(Sprite)
+  INX
+  .endrepeat
+  BNE :-
+
+  SCREEN_OFF
+
+  JSR load_palettes
+
+  JSR load_default_chr
+
+  LDA PPUSTATUS
+  LDA #$20
+  STA PPUADDR
+  LDA #$00
+  STA PPUADDR
+
+  LDA #<nametable_title
+  STA rle_ptr
+  LDA #>nametable_title
+  STA rle_ptr+1
+  JSR unrle
+
+  VBLANK
+
+  SCREEN_ON
+
+  RTS
+.endproc
+
+.proc go_to_playing
+  LDA #game_states::playing
+  STA game_state
+
+  ; erase sprites
+  LDX #$00
+  LDA #$F0
+:
+  STA oam_sprites+Sprite::ycoord, X
+  .repeat .sizeof(Sprite)
+  INX
+  .endrepeat
+  BNE :-
+
+  SCREEN_OFF
+
+  JSR load_palettes
+
+  JSR load_default_chr
+
+  LDA PPUSTATUS
+  LDA #$20
+  STA PPUADDR
+  LDA #$00
+  STA PPUADDR
+
+  LDA #<nametable_main
+  STA rle_ptr
+  LDA #>nametable_main
+  STA rle_ptr+1
+  JSR unrle
+
+  ; game setup
+
+  VBLANK
+
+  SCREEN_ON
+
+  RTS
+.endproc
+
+.proc go_to_game_over
+  LDA #game_states::game_over
+  STA game_state
+
+  ; erase sprites
+  LDX #$00
+  LDA #$F0
+:
+  STA oam_sprites+Sprite::ycoord, X
+  .repeat .sizeof(Sprite)
+  INX
+  .endrepeat
+  BNE :-
+
+  SCREEN_OFF
+
+  VBLANK
+
+  SCREEN_ON
+
+  RTS
+.endproc
+
+.proc waiting_to_start
+  JSR readjoy
+  LDA pressed_buttons
+  AND #BUTTON_START
+  BEQ :+
+  JSR go_to_playing
+:
+  RTS
+.endproc
+
+.proc game_over
+  JSR readjoy
+  LDA pressed_buttons
+  BEQ :+
+  JSR go_to_title
+:
+  RTS
+.endproc
+
+.proc playing
+  JSR readjoy
+  RTS
+.endproc
+
+.proc write_string
+  LDA PPUSTATUS
+  LDA ppu_addr_ptr+1
+  STA PPUADDR
+  LDA ppu_addr_ptr
+  STA PPUADDR
+  LDY #$00
+@loop:
+  LDA (addr_ptr), Y
+  CMP #$ff
+  BEQ @exit
+  STA PPUDATA
+  INY
+  JMP @loop
+@exit:
+  RTS
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "RODATA"
+
+.define game_state_handlers waiting_to_start-1, playing-1, game_over-1
+
+game_state_handlers_l: .lobytes game_state_handlers
+game_state_handlers_h: .hibytes game_state_handlers
+
+palettes:
+.incbin "../assets/bg-palettes.pal"
+.incbin "../assets/sprite-palettes.pal"
+
+nametable_title: .incbin "../assets/nametables/title.rle"
+nametable_main: .incbin "../assets/nametables/main.rle"
+
+; 256 pairs of points x, y in a circle
+
+.include "circle-lut.inc"
+
+.segment "CHR"
+.incbin "../assets/chr/bg-4k.chr"
+.incbin "../assets/chr/sprites-4k.chr"
+
+; 1k blocks
+;  0 : bg
+;  1 : bg
+;  2 : bg
+;  3 : bg
+;  4 : sp
+;  5 : sp
+;  6 : sp
+;  7 : sp
+
+; banks
+; 0 : 2k bg
+; 1 : 2k bg
+; 2 : 1k sp
+; 3 : 1k sp
+; 4 : 1k sp
+; 5 : 1k sp
